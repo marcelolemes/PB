d8aba86a3c29fbdf4ba5aab78239810f7cf7370a
marcelolemes <lemmarcelo@gmail.com>
2015-05-15 08:35:15 -0300

    Primeiro teste

--- PB/Form1.Designer.cs
+++ PB/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PB
 {
-    partial class Form1
+    partial class PB
     {
         /// <summary>
         /// Required designer variable.
@@ -28,29 +28,39 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PB));
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // listBox1
             // 
             this.listBox1.AllowDrop = true;
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(9, 12);
+            this.listBox1.Location = new System.Drawing.Point(7, 10);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(267, 228);
+            this.listBox1.Size = new System.Drawing.Size(201, 184);
             this.listBox1.TabIndex = 0;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.listBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.listBox1_DragDrop);
             this.listBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.listBox1_DragEnter);
+            this.listBox1.DragLeave += new System.EventHandler(this.listBox1_DragLeave);
             // 
-            // Form1
+            // PB
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(282, 255);
+            this.ClientSize = new System.Drawing.Size(212, 207);
             this.Controls.Add(this.listBox1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(228, 245);
+            this.MinimumSize = new System.Drawing.Size(228, 245);
+            this.Name = "PB";
+            this.Text = "PB";
+            this.TopMost = true;
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
--- PB/Form1.cs
+++ PB/Form1.cs
@@ -11,11 +11,11 @@ using System.Windows.Forms;
 
 namespace PB
 {
-    public partial class Form1 : Form
+    public partial class PB : Form
     {
         List<String> imagens = new List<string>();
-
-        public Form1()
+        static Bitmap newBitmap;
+        public PB()
         {
             InitializeComponent();
         }
@@ -27,17 +27,42 @@ namespace PB
 
         private void listBox1_DragEnter(object sender, DragEventArgs e)
         {
-            Bitmap image;
-            imagens.Clear();
-            listBox1.Items.Clear();
-            imagens.AddRange((String[])e.Data.GetData(DataFormats.FileDrop));
-           foreach(String s in imagens){
-               image = MakeGrayscale(new Bitmap(s));
-               image.Save(s.Substring(0,s.Length-4) + " PB.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
-               image.Dispose();
-               listBox1.Items.Add(s);
-           }
-           imagens.Clear();
+            
+            try
+            {
+                
+                listBox1.Items.Clear();
+                listBox1.Items.Add("Por favor, aguarde!!!");
+                
+                imagens.AddRange((String[])e.Data.GetData(DataFormats.FileDrop));
+
+                if (imagens.Count > 30)
+                {
+                    MessageBox.Show("Por favor, selecione menos de 30 arquivos para um melhor desempenho");
+                    imagens.Clear();
+                    listBox1.Items.Clear();
+                }
+                else {
+
+                   listBox1.Items.Clear();
+                foreach (String s in imagens)
+                {
+                    Bitmap temp = new Bitmap(s);
+                    MakeGrayscale(temp).Save(s.Substring(0, s.Length - 4) + "PB.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+                    temp.Dispose();
+                    newBitmap.Dispose();
+                    listBox1.Items.Add(s);
+                    
+                }
+                imagens.Clear();
+                listBox1.Items.Add("Pronto!!!");
+
+                }
+            }
+            catch{
+                MessageBox.Show("Um erro ocorreu, confira o que deu errado, e tente novamente");
+            }
+            
         }
 
 
@@ -45,7 +70,7 @@ namespace PB
         public static Bitmap MakeGrayscale(Bitmap original)
         {
             //create a blank bitmap the same size as original
-            Bitmap newBitmap = new Bitmap(original.Width, original.Height);
+            newBitmap = new Bitmap(original.Width, original.Height);
             newBitmap.SetResolution(300.0F, 300.0F);
             //get a graphics object from the new image
             Graphics g = Graphics.FromImage(newBitmap);
@@ -79,6 +104,18 @@ namespace PB
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
+
+        }
+
+        private void listBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            
+        }
+
+        private void listBox1_DragLeave(object sender, EventArgs e)
+        {
+
+
 
         }
     }
