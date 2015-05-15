809840cb042782944d28914073c8d2278ff3265f
marcelolemes <lemmarcelo@gmail.com>
2015-05-15 07:46:26 -0300

    Primeiro Commit

---
+++ PB/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PB
+{
+    public partial class Form1 : Form
+    {
+        List<String> imagens = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            Bitmap image;
+            imagens.Clear();
+            listBox1.Items.Clear();
+            imagens.AddRange((String[])e.Data.GetData(DataFormats.FileDrop));
+           foreach(String s in imagens){
+               image = MakeGrayscale(new Bitmap(s));
+               image.Save(s.Substring(0,s.Length-4) + " PB.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+               image.Dispose();
+               listBox1.Items.Add(s);
+           }
+           imagens.Clear();
+        }
+
+
+
+        public static Bitmap MakeGrayscale(Bitmap original)
+        {
+            //create a blank bitmap the same size as original
+            Bitmap newBitmap = new Bitmap(original.Width, original.Height);
+            newBitmap.SetResolution(300.0F, 300.0F);
+            //get a graphics object from the new image
+            Graphics g = Graphics.FromImage(newBitmap);
+
+            //create the grayscale ColorMatrix
+            ColorMatrix colorMatrix = new ColorMatrix(
+               new float[][] 
+      {
+         new float[] {.3f, .3f, .3f, 0, 0},
+         new float[] {.59f, .59f, .59f, 0, 0},
+         new float[] {.11f, .11f, .11f, 0, 0},
+         new float[] {0, 0, 0, 1, 0},
+         new float[] {0, 0, 0, 0, 1}
+      });
+
+            //create some image attributes
+            ImageAttributes attributes = new ImageAttributes();
+
+            //set the color matrix attribute
+            attributes.SetColorMatrix(colorMatrix);
+
+            //draw the original image on the new image
+            //using the grayscale color matrix
+            g.DrawImage(original, new Rectangle(0, 0, original.Width, original.Height),
+               0, 0, original.Width, original.Height, GraphicsUnit.Pixel, attributes);
+
+            //dispose the Graphics object
+            g.Dispose();
+            return newBitmap;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
